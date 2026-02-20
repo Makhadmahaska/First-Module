ce8e765d9271ab2c538e688a2f20c667ae203cb2
Makhadmahaska <mahadmahaska@hotmail.com>
2026-02-20 15:54:15 +0100

    Cache DOM elements

--- Task.ts
+++ Task.ts
@@ -14,3 +14,10 @@ interface Task {
     createdAt: Date;
     category?: string;
 }
+
+const taskForm = document.getElementById("task-form") as HTMLFormElement;
+const taskTitle = document.getElementById("task-title") as HTMLInputElement;
+const taskDesc = document.getElementById("task-desc") as HTMLTextAreaElement;
+const taskPriority = document.getElementById("task-priority") as HTMLSelectElement;
+const taskList = document.getElementById("task-list") as HTMLDivElement;
+const taskFilters = document.getElementById("task-filters") as HTMLDivElement;
