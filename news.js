faa3fa2d63fc6dd4531feba62b5639fefe81d2d9
Makhadmahaska <mahadmahaska@hotmail.com>
2026-02-20 15:03:52 +0100

    added my url and key

---
+++ news.js
@@ -0,0 +1,4 @@
+// I have found news api and use this one it says tesla news but it uppdates by itself
+
+const NEWS_API_KEY = "46ca16545d2e403eacb1c23eb241f468"; // could i have used env but i tried alot time to get working i dont wanna change to much
+const NEWS_URL = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-20&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`;
