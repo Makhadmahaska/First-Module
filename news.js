13ea0f873a66373c119bcb38db7d3da898a7f8e1
Makhadmahaska <mahadmahaska@hotmail.com>
2026-02-20 15:08:29 +0100

    add fetchNewsWithCallback function with error handling

--- news.js
+++ news.js
@@ -2,3 +2,11 @@
 
 const NEWS_API_KEY = "46ca16545d2e403eacb1c23eb241f468"; // could i have used env but i tried alot time to get working i dont wanna change to much
 const NEWS_URL = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-20&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`;
+
+
+function fetchNewsWithCallback(callback) {
+  fetch(NEWS_URL)
+    .then(response => response.json())
+    .then(data => callback(data))
+    .catch(err => console.error("Error fetching news (callback):", err));
+}
